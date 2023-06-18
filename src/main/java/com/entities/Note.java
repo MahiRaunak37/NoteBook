fd14c72be797bbaf7cbc0ee76dff0f20c096e952
MahiRaunak37 <ranjanashish374757@gmail.com>
2023-06-18 22:45:53 +0530

    navbar modification

---
+++ src/main/java/com/entities/Note.java
@@ -0,0 +1,54 @@
+package com.entities;
+
+import java.sql.Date;
+import java.util.Random;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="notes")
+public class Note {
+	@Id
+	private int id;
+	private String title;
+	private String Content;
+	private Date addedDate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return Content;
+	}
+	public void setContent(String content) {
+		Content = content;
+	}
+	public Date getAddedDate() {
+		return addedDate;
+	}
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+	public Note(String title, String content, Date addedDate) {
+		super();
+		this.id = new Random().nextInt(999999999);
+		this.title = title;
+		Content = content;
+		this.addedDate = addedDate;
+	}
+	public Note() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
